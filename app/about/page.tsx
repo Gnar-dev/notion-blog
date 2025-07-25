ad0752e0c515c120698da23ac1f090f09cebb088
Gnar-dev <initiating94@gmail.com>
2025-07-25 18:25:06 +0900

    Fix : build 시 환경변수 @secret 참조하는 오류 수정

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ export default function About() {
       <div className="space-y-8">
         <h2 className="text-3xl font-bold tracking-tight">소개</h2>
 
-        <div className="space-y-4"></div>
+        <div className="space-y-4">소개문구</div>
       </div>
     </div>
   );
